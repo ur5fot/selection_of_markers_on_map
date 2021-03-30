684dee1deaccbc4b387c26f045b24a620b4e0c03
ur5fot <wagvpered@gmail.com>
2021-03-30 23:21:02 +0300

    feat  add template handlebars

--- selection_markers_map.js
+++ selection_markers_map.js
@@ -1,7 +1,19 @@
-function selectionMarkersMap({id, idMenu, menuItem , center, title, iconUrl, iconSize, zoom, accessToken}, groups) {
+function selectionMarkersMap(setting, groups) {
+    var id = setting.id;
+    var idMenu = setting.idMenu;
+    var idMenuTemplate = setting.idMenuTemplate;
+    var activeClass = setting.activeClass;
+    var activeEl = setting.activeEl;
+    var center = setting.center;
+    var iconUrl = setting.iconUrl;
+    var iconSize = setting.iconSize;
+    var zoom = setting.zoom;
+    var accessToken = setting.accessToken;
+
     var $idMenu = $('#' + idMenu);
-    var $menuItem = $('.' + menuItem).clone();
-    $('.' + menuItem).remove()
+
+    var source = document.getElementById(idMenuTemplate).innerHTML;
+    var template = Handlebars.compile(source);
 
     var map = L.map(id).setView(center, zoom);
 
@@ -15,19 +27,22 @@ function selectionMarkersMap({id, idMenu, menuItem , center, title, iconUrl, ico
     }).addTo(map);
 
     // Creating a custom icon
-    var customIconCenter = L.icon({ iconUrl: iconUrl, iconSize: iconSize });
+    var customIconCenter = L.icon({iconUrl: iconUrl, iconSize: iconSize});
 
     // Creating a marker
-    var markerCenter = new L.Marker(center, { title: title, clickable: true, draggable: false, icon: customIconCenter });
-    markerCenter.bindPopup(title).openPopup();
+    var markerCenter = new L.Marker(center, {clickable: false, draggable: false, icon: customIconCenter});
     // Adding marker to the map
     markerCenter.addTo(map);
 
-    var layers = groups.map(group => {
-      var layer = group.icons.map(markerData => {
+    var layers = groups.map((group) => {
+        var menu = group.menu
+        var iconUrl = group.iconUrl
+        var icons = group.icons
+
+        var layer = icons.map(markerData => {
             // Icon options
             var iconOptions = {
-                iconUrl: group.iconUrl,
+                iconUrl: iconUrl,
                 iconSize: iconSize
             }
 
@@ -36,43 +51,45 @@ function selectionMarkersMap({id, idMenu, menuItem , center, title, iconUrl, ico
 
             // Options for the marker
             var markerOptions = {
-                title: markerData.title,
-                clickable: true,
+                clickable: false,
                 draggable: false,
                 icon: customIcon
             }
             // Creating a marker
             var marker = new L.Marker(markerData.center, markerOptions);
 
-            // Adding pop-up to the marker
-            marker.bindPopup(markerData.title).openPopup();
             return marker
         })
+
         var layerGroup = L.layerGroup(layer);
-        if (group.default) {
+        if (menu.default) {
             layerGroup.addTo(map);
         }
-        return layerGroup
+
+        return {menu: menu, layerGroup: layerGroup}
     })
 
-    groups.map((layer, i) => {
-         $menuItem
-             .clone()
-             .append(layer.name)
-             .addClass(layer.default ? 'active' : '')
-             .on('click', layers[i], function (e) {
-                 var $this = $(this)
-                 if ($this.hasClass('active')){
-                     $this.removeClass('active')
-                     // Removing layer from map
-                     map.removeLayer(e.data);
-                 }else {
-                     $this.addClass('active')
-                     // add layer from map
-                     e.data.addTo(map);
+    layers.forEach((layer) => {
+        var menu = layer.menu
+        var layerGroup = layer.layerGroup
 
-                 }
-             })
-             .appendTo($idMenu)
+        menu.activeClass = activeClass
+        var html = template(menu);
+            $(html)
+            .on('click', layerGroup, function (e) {
+                var $this = $(this)
+                var $activeEl = $this.find('.' + activeEl)
+                if ($activeEl.hasClass(activeClass)) {
+                    $activeEl.removeClass(activeClass)
+                    // Removing layer from map
+                    map.removeLayer(e.data);
+                } else {
+                    $activeEl.addClass(activeClass)
+                    // add layer from map
+                    e.data.addTo(map);
+
+                }
+            })
+            .appendTo($idMenu)
     })
 }
